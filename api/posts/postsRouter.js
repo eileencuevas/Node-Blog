1ef3249bef9fe31518bc272d656db443014ae57a
steelworx <eileencuevas62@gmail.com>
2019-01-10 16:25:47 -0500

    added PUT endpoints for posts in postsRouter.js

--- api/posts/postsRouter.js
+++ api/posts/postsRouter.js
@@ -16,12 +16,37 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     const id = req.params.id;
-    
+
     postDb
         .get(id)
         .then(post => {
             if (post) {
                 res.status(200).json(post);
+            } else {
+                res.status(404).json({ "error": 'No post with the specified ID found.' })
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ "error": 'No information could be retrieved. '})
+        });
+})
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const newPostData = req.body;
+    
+    postDb
+        .get(id)
+        .then(post => {
+            if (post) {
+                postDb
+                    .update(id, newPostData)
+                    .then(() => {
+                        res.status(200).json({ ...newPostData });
+                    })
+                    .catch(() => {
+                        res.status(500).json({ "error": 'This post could not be updated. Please try again.' })
+                    });
             } else {
                 res.status(404).json({ "error": 'No post with the specified ID found.' })
             }
